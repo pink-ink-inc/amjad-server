659227d2fed3ce7f7b72d99769d71794da67b700
root <root@api-amjad-01.ngw>
2014-12-21 14:05:05 +0000

    A working set-up with template hashes.

--- server.py
+++ server.py
@@ -10,6 +10,23 @@ app = flask.Flask(__name__)
 def test():
     return 'test'
 
+@app.route('/api/echo', methods = ['GET', 'POST'])
+def echo():
+    return json.dumps ( definite_strategy (
+                {
+                      'GET': lambda x: flask.request.args
+                    , 'POST': lambda x: flask.request.data
+                }
+                , flask.request.method
+                , None
+                )
+            )
+
+@app.route('/api/echo-json', methods = ['POST'])
+def echo_json():
+    print type(flask.request.json)
+    return json.dumps ( flask.request.json, indent = 2 )
+
 @app.route('/api/repr/<key>')
 def repr_route(key):
     return json.dumps(repr(key), indent=2)
@@ -18,14 +35,40 @@ def repr_route(key):
 def render_route(key):
     return render(key)
 
+@app.route('/api/keys/<key>')
+def list_keys(key):
+    return json.dumps (redis.keys(key), indent=2)
+
+@app.route('/api/add/<key>', methods = ['POST'])
+def add_key(key):
+    try:
+        strategies = {
+              'string': lambda (key, value): redis.set ( key, value )
+            , 'hash': lambda (key, value): redis.hmset (key, value)
+            , 'list': lambda (key, value): [redis.rpush (key, x) for x in value] 
+            }
+
+        print (strategies)
+        print flask.request.json
+
+        return json.dumps(definite_strategy (strategies, flask.request.json['kind'], (key, flask.request.json['value']) ), indent = 2)
+    except Exception as e:
+        return str(e)
+
+@app.route('/api/del/<key>', methods = ['POST'])
+def del_key(key):
+    return json.dumps ( redis.delete(key), indent = 2 )
+
+
 def render(key):
+    # print 'rendering ' + key
     key_type = redis.type(key)
     strategies = {
               'string': 'repr_string'
             , 'list': 'render_list'
             , 'set': 'repr_set'
             , 'zset': 'repr_zset'
-            , 'hash': 'repr_hash'
+            , 'hash': 'render_hash'
             , 'none': 'repr_none'
             }
     return strategy ( strategies, key_type, key )
@@ -40,18 +83,22 @@ def repr(key):
             , 'hash': 'repr_hash'
             , 'none': 'repr_none'
             }
-    return strategy ( strategies, key_type, key )
+    return {
+              'key': key
+            , 'repr': strategy ( strategies, key_type, key )
+        }
 
 def strategy (strategies, key, value):
     for each in strategies:
         if key == each:
+            # print 'chosen strategy ' + strategies[each]
             try:
                 return globals()[strategies[each]] (value)
             except AttributeError:  # That is, the function name is not present in the namespace.
                 pass 
     return False
 
-def definite_strategy (strategies, key, value, default_strategy):
+def definite_strategy (strategies, key, value, default_strategy=''):
     for each in strategies:
         if key == each:
             try:
@@ -65,7 +112,9 @@ def repr_string(key):
 
 def repr_list(key):
     strategies = {
-              '::': repr
+                '::': repr
+              , ':#': repr
+              , ':!': repr
               , ':-': lambda x: [ repr(each) for each in redis.keys(x + ':*') ]
               , '': str
             }
@@ -73,19 +122,57 @@ def repr_list(key):
             for key in redis.lrange ( key, 0, -1 ) ]
 
 def repr_hash(key):
-    return [ ]
+    hash_ = redis.hgetall(key)
+    if 'template' in hash_.keys():
+        return dict ( [ (key, repr(hash_[key])) for key in hash_.keys() ] )
+    else:
+        return hash_
+                
 
 def repr_none(key):
-    return 'none here'
+    return ''
 
 def render_list(key):
     strategies = {
-              '::': render
+                '::': render
+              , ':#': render
               , ':-': lambda x: '\n'.join([ render(each) for each in redis.keys(x + ':*') ])
               , '': str
             }
     return '\n'.join ([ definite_strategy (strategies, key[0:2], key, '')
             for key in redis.lrange ( key, 0, -1 ) ])
+
+def render_hash(key):
+
+    def intermix (template, values):
+        for value in values:
+            for template_string in template:
+                if template_string[0:2] == ':!':
+                    s = redis.hget ( value, template_string )
+                    if s[0:2] == '::':
+                        yield render (s)
+                    else:
+                        yield s
+                else:
+                    yield template_string
+        # That would be all.
+
+    def render_hash_template(key):
+        return '\n'.join ( intermix (
+                  redis.lrange ( redis.hget ( key, 'template' ), 0, -1 )
+                , redis.lrange ( redis.hget ( key, 'values' ), 0, -1 )
+                ) )
+
+    return definite_strategy (
+                { ':#': render_hash_template
+                # , ':@': render_hash_values
+                , '': str
+                }
+                , key[0:2], key)
+
+
+    def render_hash_values(key):
+        pass
 
 
 if __name__ == '__main__':
